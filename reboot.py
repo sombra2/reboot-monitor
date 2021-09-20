9f4af23f6e4f97aed676dbe5d325644e9324d83e
sombra2 <sombra2@gmail.com>
2021-09-20 17:53:06 +0400

    branch to create version 2

--- reboot.py
+++ reboot.py
@@ -1,24 +1,31 @@
 # This script is completely functiontal as it is. It just sends a ping message to your telegram to tell
 # you that the machine has rebooted
 
+import os
 import requests
 import datetime
+import json
 # the credentials.py file is not here, you will have to create your own with the variables related to bot_token,
 # bot_chatID and ip_token for the geo ip api service
 import credentials
 
 now = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-device = 'MacBook Pro' # name here the device the way you want it to show up in your telegram message todo retrieve hostname
+device = os.uname()[1].split('.')[0] # we get the hostname without extension here
 
-def retrieve_geoipdata():
-  response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=" + credentials.ip_api)
-  print(response.content)
-
+response = requests.get('https://ipgeolocation.abstractapi.com/v1/?api_key=' + credentials.ip_api)
+response = json.loads(response.content) # parses the json object into a python dictionary
+ip_address = response['ip_address']
+city = response['city']
+emoji = response['flag']['emoji']
 
 def telegram_bot_sendtext(bot_message):
   send_text = 'https://api.telegram.org/bot' + credentials.bot_token + '/sendMessage?chat_id=' + credentials.bot_chatID + '&parse_mode=markdown&text=' + bot_message
   response = requests.get(send_text)
   return response.json()
 
-#telegram_bot_sendtext('{} at [{}] -=REBOOT=- on {}'.format(device,ip,now))
-retrieve_geoipdata()
+telegram_bot_sendtext('<b>{}</b> at {} ({} {}) -=REBOOT=- on {}'.format(
+  device,
+  ip_address,
+  city,
+  emoji,
+  now))
